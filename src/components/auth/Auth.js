4df60719acea5a58fcd92033903a42a4633c6e9a
Shixun <shixun.liu1023@gmail.com>
2018-03-09 17:45:46 +1100

    update the github login button

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -84,7 +84,7 @@ class Auth extends React.Component {
                 {getFieldDecorator('password', { rules: [{ required: true, message: 'Please input your password!' }] })(<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='Enter password...' type='password' />)}
               </FormItem>
             </Form>
-            <Button type='primary' onClick={this.handleGithubLogin}>github login</Button>
+            Login by: <Button icon='github' size='large' onClick={this.handleGithubLogin}>github</Button>
           </TabPane>
 
           <TabPane tab='Register' key='2'>
